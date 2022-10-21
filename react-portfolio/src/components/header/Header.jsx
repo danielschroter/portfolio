0b6a99452e326608e3ff186d6a307c8710728f77
Daniel Schroter <daniel@schroter.biz>
2022-10-21 16:33:07 +0200

    VH Adjustment for Socials and Scroll Down

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -5,26 +5,26 @@ import ME from "../../assets/me-5.png"
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return (
-      <header>
-          <div className="container header__container">
-              <h5>Hello I'm</h5>
-              <h1>Daniel Schroter</h1>
-              <h5 className="text-light">Data Scientist & AI Engineer</h5>
-              <CTA></CTA>
-              <HeaderSocials/>
+    return (
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Daniel Schroter</h1>
+                <h5 className="text-light">Data Scientist & AI Engineer</h5>
+                <CTA></CTA>
+                <HeaderSocials/>
 
-              <div className="me">
-                  <img src={ME} alt="me" />
+                <div className="me">
+                    <img src={ME} alt="me"/>
 
-              </div>
-              <a href="#contact" className='scroll__down'>Scroll Down</a>
+                </div>
+                <a href="#contact" className='scroll__down'>Scroll Down</a>
 
-          </div>
+            </div>
 
 
-      </header>
-  )
+        </header>
+    )
 }
 
 export default Header
